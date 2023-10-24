5fa2addeedc697a841d51f4ae913eeaa07924aca
Alexandr Zubarev <zualvi@yandex.ru>
2023-10-24 18:52:26 +0300

    Fixed create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,5 +1,5 @@
 -- SQL-команды для создания таблиц
-CREATE TABLE employees_data
+CREATE TABLE employees
 (
 	employee_id serial PRIMARY KEY,
 	first_name varchar(30) NOT NULL,
@@ -9,18 +9,18 @@ CREATE TABLE employees_data
 	notes text
 );
 
-CREATE TABLE customers_data
+CREATE TABLE customers
 (
 	customer_id varchar(5) PRIMARY KEY,
 	company_name varchar(50) NOT NULL,
 	contact_name varchar(50) NOT NULL
 );
 
-CREATE TABLE orders_data
+CREATE TABLE orders
 (
 	order_id serial PRIMARY KEY,
-	customer_id varchar(5) NOT NULL REFERENCES customers_data(customer_id),
-	employee_id int NOT NULL REFERENCES employees_data(employee_id),
+	customer_id varchar(5) NOT NULL REFERENCES customers(customer_id),
+	employee_id int NOT NULL REFERENCES employees(employee_id),
 	order_date date NOT NULL,
 	ship_city varchar(30) NOT NULL
 );
